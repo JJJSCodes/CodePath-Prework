8c2971007f199fdbe49cb9a3d0133ae69589cee5
JJJSCodes <meijason15@gmail.com>
2023-02-05 16:25:35 -0800

    Initial Commit

---
+++ Introduce Student/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Introduce Student
+//
+//  Created by Jason Mei on 2/5/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
